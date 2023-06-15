b67bf800c4f00b143435f844cf54858d6b1cc38a
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2023-06-14 23:45:53 -0300

    Add files via upload

--- lib/data/daos/user_dao.dart
+++ lib/data/daos/user_dao.dart
@@ -1,17 +1,22 @@
 import 'dart:async';
 
 import 'package:drift/drift.dart';
+import '../../UI/screens/register.dart';
 import '../database.dart';
 import '../models/user.dart';
 
 part 'user_dao.g.dart';
 
 @DriftAccessor(tables: [User])
-abstract class UserDao extends DatabaseAccessor<MyDatabase> with _$UserDaoMixin {
+class UserDao extends DatabaseAccessor<MyDatabase> with _$UserDaoMixin {
   UserDao(MyDatabase db) : super(db);
 
 
-  Future<int> addUser(User user) => into(db.user).insert(user as Insertable<UserData>);
+  Future<int> addUser(IUser user) => into(db.user).insert(UserCompanion(
+    name: Value(user.name),
+    email: Value(user.email),
+    password: Value(user.password),
+  ));
 
   Future<List> getAllUsers() => select(db.user).get();
 
