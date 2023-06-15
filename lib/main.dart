41137207fdc0f27712cb6ce7fc4018c7c6bbf032
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2023-06-14 23:47:05 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:taskapp/UI/screens/login.dart';
 
+import 'data/database.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<MyDatabase>(
+          create: (_) => MyDatabase(),
+        ),
+        // Outros providers, se houver
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Login(),
 
     );
